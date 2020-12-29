e7a9354534b12bb21f4cfec28a669a3cccc6de44
shivaramkhanal <55666186+shivaramkhanal@users.noreply.github.com>
2020-12-29 11:19:07 -0500

    Update man.tf

--- man.tf
+++ man.tf
@@ -1,20 +1,67 @@
+  resource "aws_instance" "web3" {
+  ami = "${var.ami_image}"
+  instance_type = "${var.instance_type}"
+  key_name = "${var.key_name}"
+  vpc_security_group_ids = ["${aws_security_group.securitygr.name}"]
+  user_data       = "${file("userdata.sh")}"
+  iam_instance_profile = "${aws_iam_instance_profile.test_profile.name}"
+ }
 provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "web1" {
-  ami = "${var.ami_image}"
-  instance_type = "${var.instance_type}"
-  key_name = "${var.key_name}"
-  security_groups = ["${var.security_groups}"]
-  user_data       = "${file("userdata.sh")}"
-  iam_instance_profile = "${aws_iam_instance_profile.test_profile.name}"
-  tags {
-    Name = "Instance1"
-  }
- }
 
-resource "aws_iam_instance_profile" "test_profile" {                             
-name = "profile"                         
-role = "admin"
+resource "aws_iam_instance_profile" "test_profile" {                                                    
+#role = "admin"
 }  
+
+resource "aws_security_group" "securitygr" {
+  name        = "security2"
+  description = "create security group"
+  
+  
+  
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+   ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+
+  egress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+   egress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
+}
